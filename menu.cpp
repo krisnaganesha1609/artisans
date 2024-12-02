9604a73dcb9cf0392f3684bbd5439ea51c8d8c87
riarumoda <justinebudiwijaya@gmail.com>
2024-12-02 08:19:43 +0800

    add menu sub-header and sub-source files

---
+++ menu.cpp
@@ -0,0 +1,16 @@
+#include "menu.h"
+
+void welcomeBanner() {
+    cout << "                                          \n" << endl;
+    cout << "     _         _   _                      \n" << endl; 
+    cout << "    / \   _ __| |_(_)___  __ _ _ __  ___  \n" << endl;
+    cout << "   / _ \ | '__| __| / __|/ _` | '_ \/ __| \n" << endl;
+    cout << "  / ___ \| |  | |_| \__ \ (_| | | | \__ \ \n" << endl;
+    cout << " /_/   \_\_|   \__|_|___/\__,_|_| |_|___/ \n" << endl;
+    cout << "                                          \n" << endl;
+    cout << "==========================================\n" << endl;
+    cout << " Artisans                   Business Tool \n" << endl;
+    cout << " Made by:      Gede Krisna & Putu Justine \n" << endl;
+    cout << " Available on:            Windows & Linux \n" << endl;
+    cout << "==========================================\n" << endl;
+}
